d340ded57bafbc6fa952b421c8c98760f85e94b4
Mari Shchepina <marishchepina@gmail.com>
2021-04-15 13:07:38 +0300

    Smooth scrolling

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*-----------------  Smooth scrolling ----------------------------*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // Oldschool way
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
