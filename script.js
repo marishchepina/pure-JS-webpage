8b4a0a9d7dff72acf6207a6024d94c61e660cb6e
Mari Shchepina <marishchepina@gmail.com>
2021-04-16 08:56:13 +0300

    navigation

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,18 +34,32 @@ document.addEventListener('keydown', function (e) {
 
 /*-----------------  Smooth scrolling ----------------------------*/
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
-
+  section1.scrollIntoView({ behavior: 'smooth' });
   // Oldschool way
   // window.scrollTo({
   //   left: s1coords.left + window.pageXOffset,
   //   top: s1coords.top + window.pageYOffset,
   //   behavior: 'smooth',
   // });
-
-  section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+/*-----------------     Navigation    ----------------------------*/
+//event delegation way
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//It works, but it's hard for the processor
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
