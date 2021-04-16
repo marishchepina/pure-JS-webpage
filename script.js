7f4a2cce22f730cb740d134e3d23fb021e31dca7
Mari Shchepina <marishchepina@gmail.com>
2021-04-16 10:49:44 +0300

    tabbed component

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
 
+/*---------------------   Modal window   ------------------------------------*/
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -37,6 +38,7 @@ document.addEventListener('keydown', function (e) {
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   section1.scrollIntoView({ behavior: 'smooth' });
+
   // Oldschool way
   // window.scrollTo({
   //   left: s1coords.left + window.pageXOffset,
@@ -55,7 +57,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
-//It works, but it's hard for the processor
+//It works, but it's hard for the cpu
 // document.querySelectorAll('.nav__link').forEach(function (el) {
 //   el.addEventListener('click', function (e) {
 //     e.preventDefault();
@@ -63,3 +65,16 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 //     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
 //   });
 // });
+
+/*----------------------  Tabbed component  ----------------------------*/
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab'); //span click + parent  button click(event delegation)
+
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
