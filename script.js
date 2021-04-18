58b75f2e4189b3c521c0ef3272158c8ed2859704
Mari Shchepina <marishchepina@gmail.com>
2021-04-18 04:57:46 +0300

    reveal sections

--- script.js
+++ script.js
@@ -131,3 +131,21 @@ headerObserver.observe(header);
 //     nav.classList.remove('sticky');
 //   }
 // });
+
+/*----------------------  Reveal sections  ----------------------------*/
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); // scroll  event is only the first time
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
