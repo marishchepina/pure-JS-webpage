f86319d356650c2bbb2af1072d0dc0d70c99c397
Mari Shchepina <marishchepina@gmail.com>
2021-04-18 03:58:49 +0300

    sticky navigation

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const tabsContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 /*---------------------   Modal window   ------------------------------------*/
 const openModal = function (e) {
@@ -48,10 +49,10 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
 });
 
-/*-----------------     Navigation    ----------------------------*/
+/*--------------------------     Navigation    ----------------------------*/
 //event delegation way
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
+  e.preventDefault(); // prevent default html scrolling to the ancor
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
@@ -87,7 +88,6 @@ const handleHover = function (e) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
-    console.log(siblings);
     siblings.forEach(el => {
       if (el !== link) el.style.opacity = this; // this - opacity
     });
@@ -106,3 +106,28 @@ const handleHover = function (e) {
 //Passing "argument" into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+/*----------------------  Sticky navigation  ----------------------------*/
+
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// Old way
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function (e) {
+//   if (window.scrollY > initialCoords.top) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// });
