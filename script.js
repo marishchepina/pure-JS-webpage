866ebfacb05d2421c610143884b0a80541439d0e
Mari Shchepina <marishchepina@gmail.com>
2021-04-26 21:11:12 +0300

    6-module-1-task test error

--- script.js
+++ script.js
@@ -146,8 +146,8 @@ const sectionObserver = new IntersectionObserver(revealSection, {
   threshold: 0.15,
 });
 allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  //sectionObserver.observe(section);
+  //section.classList.add('section--hidden');
 });
 
 /*----------------------  Lazy loading images  ----------------------------*/
@@ -170,3 +170,44 @@ const imgObserver = new IntersectionObserver(loadImg, {
   rootMargin: '200px',
 });
 imgTargets.forEach(img => imgObserver.observe(img));
+
+/*----------------------  Slider  ----------------------------*/
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+//slider.style.transform = 'scale(0.4) translateX(-800px)';
+//slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
+  });
+};
+
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
